777b7b8b9243604aa6d1ea6a72d038b73a413a7d
Kenta Sato <tosainu.maple@gmail.com>
2019-09-06 22:37:36 +0900

    Brainf**k コンパイラで動作確認

--- src/bin/chiya.rs
+++ src/bin/chiya.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, World!");
+use std::io::Read;
+
+use chiya::codegen::emitter::Emitter;
+use chiya::codegen::llvm::LLVM;
+
+fn main() -> std::io::Result<()> {
+    let mut src = String::new();
+    std::io::stdin().read_to_string(&mut src)?;
+
+    let mut e = LLVM::new();
+
+    println!("{}", e.emit_header());
+    for c in src.chars() {
+        let l = match c {
+            '>' => e.emit_move_ptr(1),
+            '<' => e.emit_move_ptr(-1),
+            '+' => e.emit_add(1),
+            '-' => e.emit_add(-1),
+            '.' => e.emit_call_putchar(),
+            ',' => e.emit_call_getchar(),
+            '[' => e.emit_loop_begin(),
+            ']' => e.emit_loop_end(),
+            _ => continue,
+        };
+        println!("{}", l);
+    }
+    println!("{}", e.emit_footer());
+
+    Ok(())
 }
